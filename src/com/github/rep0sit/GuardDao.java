b5b799704870e42b95008f55b855664435c900cf
etienne0n <etienneonasch@web.de>
2018-05-03 10:17:34 +0200

    GuardDao geändert. Jetzt mit globalVar

--- src/com/github/rep0sit/GuardDao.java
+++ src/com/github/rep0sit/GuardDao.java
@@ -1,93 +1,28 @@
 package com.github.rep0sit;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.HashSet;
 import java.util.Set;
 
 public class GuardDao {
-	private static String FILE_NAME = "guards.dat";
-	@SuppressWarnings("unchecked")
-	public void register(Guard guard){
-		Set<Guard> guardSet = null;
-		
-		try {
-			File file = new File(FILE_NAME);
-			if (!file.exists()) {
-
-				guardSet = new HashSet<Guard>();
-				guardSet.add(guard);
-				saveguardList(guardSet);
-			} else {
-				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
-				guardSet = (Set<Guard>) ois.readObject();
-				ois.close();
-
-				guardSet.add(guard);
-				saveguardList(guardSet);
-			}
-
-		} 
-		
-		catch (IOException e) {e.printStackTrace();} 
-		catch (ClassNotFoundException e) {e.printStackTrace();}
-		
-	}
+private static Set<Guard> guards = new HashSet<>();
 	
 	
-	@SuppressWarnings("unchecked")
 	public Set<Guard> getAllGuards(){
-		
-		Set<Guard> guardSet = null;
-		
-		try {
-			//File file = new File(FILE_NAME);
-			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME));
-			guardSet = (Set<Guard>) ois.readObject();
-			ois.close();
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		return guardSet;
-	}
-
-	private void saveguardList(Set<Guard> guardList){
-		try {
-			File file = new File(FILE_NAME);
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-			oos.writeObject(guardList);
-			oos.close();
-		}
-		
-		catch(FileNotFoundException e) {e.printStackTrace();}
-		catch(IOException e) {e.printStackTrace();}
+		return guards;
 		
 	}
 
+	
 
 	public Guard getGuard(int id) {
-		Set<Guard> guards = getAllGuards();
-		for(Guard guard : guards) {
-			if(guard.getId() == id) {
-				return guard;
-			}
-		}
-		return null;
+		return guards.stream().filter(g -> g.getId() == id).findAny().orElse(null);
 	}
+	
 
 
 	public int addGuard(Guard guard) {
-		Set<Guard> guardList = getAllGuards();
-		if(guardList.contains(guard)) {return 0;}
-		guardList.add(guard);
+		if(guards.contains(guard)) {return 0;}
+		guards.add(guard);
 		return 1;
 	}
 }
